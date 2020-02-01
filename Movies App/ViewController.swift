082ff8a81907b98bb314c21cfed9181b60ccb1e8
Justin Armstrong <justinjustarms48@gmail.com>
2020-02-01 14:22:12 -0500

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Justin Armstrong on 2/1/20.
+//  Copyright © 2020 Justin Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Movies AppTests/Movies_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Movies_AppTests.swift
+//  Movies AppTests
+//
+//  Created by Justin Armstrong on 2/1/20.
+//  Copyright © 2020 Justin Armstrong. All rights reserved.
+//
+
+import XCTest
+@testable import Movies_App
+
+class Movies_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
