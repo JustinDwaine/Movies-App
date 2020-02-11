6a5df1f34ef8775ad359b7df1dcfea494408d9bc
Justin Armstrong <justinjustarms48@gmail.com>
2020-02-10 22:25:30 -0500

    Part 2

---
+++ Movies App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Movies App
+//
+//  Created by Justin Armstrong on 2/10/20.
+//  Copyright © 2020 Justin Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
